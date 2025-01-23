97053ee11ff375c43afddf24e29be733b7a27c4e
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-01-23 23:14:00 +0000

    updating the eval function

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,56 @@
-import { useState } from 'react';
-import './App.css'
-import Buttons from './Components/Buttons';
-import Input from './Components/Input'
+import { useState } from "react";
+import "./App.css";
+import Buttons from "./Components/Buttons";
+import Input from "./Components/Input";
 
 function App() {
-
   const [calval, setCalVal] = useState("");
   const handleOnClickEvent = (e) => {
     let pressed = e.target.textContent;
+
     if (pressed === "AC") {
       setCalVal("");
-    }
-    else if (pressed === "=") {
-      const res = eval(calval);
-      setCalVal(res);
-    }
-    else if (pressed === "()") {
+    } else if (pressed === "=") {
+      try {
+        const res = eval(calval);
+        setCalVal(res.toString());
+      } catch (err) {
+        setCalVal("Error");
+      }
+    } else if (pressed === "()") {
       let newVal = calval;
       let openParens = (calval.match(/\(/g) || []).length;
       let closeParens = (calval.match(/\)/g) || []).length;
       if (openParens > closeParens) {
         newVal += ")";
       } else {
+        if (/\d$/.test(newVal)) {
+          newVal += "*";
+        }
         newVal += "(";
       }
       setCalVal(newVal);
-    }
-
-    else if (pressed === "⌦") {
+    } else if (pressed === "⌦") {
       let newTextContent = calval.slice(0, -1);
       setCalVal(newTextContent);
-    }
-    else {
-      const newVal = calval + pressed;
+    } else {
+      let newVal = calval;
+      if (/\)$/.test(newVal) && /\d/.test(pressed)) {
+        newVal += "*";
+      }
+      newVal += pressed;
       setCalVal(newVal);
     }
+  };
 
-  }
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <Input calval={calval} />
         <Buttons handleOnClickEvent={handleOnClickEvent} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
